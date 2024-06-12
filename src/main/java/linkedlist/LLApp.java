9093c378e4d43ee20d29052808e49c2698dc903c
maryRom01 <maryna_romanets@aol.com>
2024-06-11 22:39:19 -0400

    refactor LLApp

--- src/main/java/linkedlist/LLApp.java
+++ src/main/java/linkedlist/LLApp.java
@@ -4,6 +4,23 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class LLApp {
+
+    public static XLinkedList getRandomArray() {
+        int[] randoms = new Random().ints(20, 1, 100)
+                .toArray();
+        XLinkedList ll = new XLinkedList();
+        for (int r: randoms) {
+            ll.insertSorted(r);
+        }
+        System.out.println(ll);
+        return ll;
+    }
+
+    public static void printList(XLinkedList ll) {
+        System.out.println(ll);
+        System.out.println("----------------------------------------------");
+    }
+
     public static void main(String[] args) {
 //        XLinkedList ll = new XLinkedList();
 //
@@ -27,24 +44,19 @@ public class LLApp {
 //        ll.insertSorted(100);
 //        System.out.println(ll); // [0] -> [1] -> [3] -> [4] -> [5] -> [6] -> [9] -> [10] -> [100]
 
-//        System.out.println(ll.reverse());
-//        System.out.println(ll);
+//        int[] randoms = new Random().ints(20, 1, 100)
+//                .toArray();
 
-//        ll.reverseR();
-//        System.out.println(ll);
+        XLinkedList ll = getRandomArray();
+                System.out.println(ll.reverse());
+        printList(ll);
 
-        int[] randoms = new Random().ints(20, 1, 100)
-                .toArray();
+        ll = getRandomArray();
+        ll.reverseR();
+        printList(ll);
 
-        System.out.println(Arrays.toString(randoms));
-        XLinkedList ll = new XLinkedList();
-        for (int r: randoms) {
-            ll.insertSorted(r);
-        }
-
-        System.out.println(ll);
-
+        ll = getRandomArray();
         ll.reverseR2();
-        System.out.println(ll);
+        printList(ll);
     }
 }
